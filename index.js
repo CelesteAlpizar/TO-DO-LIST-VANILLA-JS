e91752233a9eefaed20f0048bd6b65dfa47b0b69
María Celeste Alpízar Solano <maria.alpizar.72@gmail.com>
2021-05-24 20:18:52 +0000

    Almost ready

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const form = document.getElementById("myform")
 const input = document.getElementById("input")
 const tasklist = document.getElementById("tasks-list")
-const template = document.getElementById("template-tasks").content // siempre recordar el .content al final de los templates
+const template = document.getElementById("template-tasks").content // siempre recordar el .content al final de los templates para acceder al contenido
 const fragment = document.createDocumentFragment()
 let tasks = {}
 
 document.addEventListener("DOMcContentLoaded", () => {
-    showtask()
+    if (localStorage.getItem("tasks")) {
+        tasks = JSON.parse(localStorage.getItem("tasks"))
+    }
+    showtasks()
 })
 
 tasklist.addEventListener("click", e => {
@@ -18,13 +21,13 @@ tasklist.addEventListener("click", e => {
 
 form.addEventListener("submit", e => {
     e.preventDefault()
-    console.log(input.value)
+    // console.log(input.value)
     setTask(e)
 })
 
 const setTask = e => {
     if(input.value.trim() === ""){
-        // console.log("There's not data")
+        // console.log("There"s not data")
         return
     }
     const task = {
@@ -35,17 +38,62 @@ const setTask = e => {
     tasks[task.id] = task
     form.reset()
     input.focus()
-    showtask()
+    showtasks()
 }
 
-const showtask = () => {
+const showtasks = () => {
+
+    localStorage.setItem("tasks", JSON.stringify(tasks))
+
+      if (Object.values(tasks).length === 0) {
+        tasklist.innerHTML = `
+        <div class="alert alert-info m-0 py-1 text-center">
+        There's no pending tasks!
+        </div>
+        `
+        return
+    }
+
     tasklist.innerHTML = ""
     Object.values(tasks).forEach(task =>{
         const clone = template.cloneNode(true)
         clone.querySelector("p").textContent = task.text
+
+        if (task.state) {
+            clone.querySelector(".alert").classList.replace("alert-warning", "alert-primary")
+            clone.querySelectorAll(".fas")[0].classList.replace("fa-check", "fa-undo-alt")
+            clone.querySelector("p").style.textDecoration = "line-through"
+        }
+        // se usó el clone aqui para evitar que use el template editado de una tarea anterior
+
+        clone.querySelectorAll(".fas")[0].dataset.id = task.id
+        clone.querySelectorAll(".fas")[1].dataset.id = task.id
         fragment.appendChild(clone)
     })
     tasklist.appendChild(fragment)
 }
 
-const btnAction = e =>{}
+const btnAction = e =>{
+    // console.log(e.target.classList.contains("fa-check"))
+    if(e.target.classList.contains("fa-check")){
+        // console.log(e.target.dataset.id)
+        tasks[e.target.dataset.id].state = true
+        showtasks()
+        // console.log(tasks)
+    }
+
+    if(e.target.classList.contains("fa-trash-alt")){
+        delete tasks[e.target.dataset.id]
+        showtasks()
+        // console.log(tasks)
+    }
+
+    if(e.target.classList.contains("fa-undo-alt")){
+        tasks[e.target.dataset.id].state = false
+        showtasks()
+        // console.log(tasks)
+    }
+
+
+    e.stopPropagation()
+}
